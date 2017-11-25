c1a3b10cde40b5adf33a655db23bedcc6e82758c
Abdalla Elmedny <abdelelmedny@gmail.com>
2017-11-25 21:15:45 +0100

    chore(initial): initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+require('ts-node/register');
+
+const Main = require('./lib/main').Main;
+
+const main = new main();
---
+++ lib/main.ts
@@ -0,0 +1,6 @@
+export class Main {
+
+    constructor() {
+        console.log("Main constructed");
+    }
+}
