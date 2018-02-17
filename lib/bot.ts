fefd2c0cb91d76723321d641e718c89b368d74c1
Abdel El-Medny <abdel@springtree.nl>
2018-02-17 21:28:48 +0100

    feat(bot): adds bot code to send apartament summary

--- lib/bot.ts
+++ lib/bot.ts
@@ -1,9 +1,38 @@
 import * as TelegramBot from 'node-telegram-bot-api';
+import { IApartamentSummary, Apartament } from './rooms';
 
 /** Telegram bot token */
 const token = process.env['TELEGRAM_BOT_TOKEN'];
 
 export class Bot {
-    constructor() {
+
+    private botInstance: TelegramBot;
+
+    /** Telegram account ids of those who are interested in the bots content */
+    private readonly RECEIVER_IDS = [21352993];
+
+    constructor() { 
+        console.log('token is', token);
+
+        this.botInstance = new TelegramBot(token);
+
+        this.botInstance.onText(/\/debug/, msg => {
+            this.botInstance.sendMessage(msg.chat.id, JSON.stringify(msg, null, 4));
+        });
+    }
+
+    /** Sends a report summary via telegram */
+    public sendApartamentSummary( apartament: IApartamentSummary  ) {
+
+        this.RECEIVER_IDS.forEach( id => {
+            const { latt, lang } = apartament.location;
+
+            this.botInstance.sendVenue(id, latt, lang, apartament.price, apartament.adress);
+
+            this.botInstance.sendMessage(id, `${apartament.description} [hier](${apartament.url}`, {
+                parse_mode: 'Markdown'
+            });
+        });
+
     }
 }
