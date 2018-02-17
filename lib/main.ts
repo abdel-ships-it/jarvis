6dd74d25b5ea3f7fa76a62091f9cd30405688812
Abdel El-Medny <abdel@springtree.nl>
2018-02-17 21:28:58 +0100

    feat(main): adds cron job which checks new apartaments

--- lib/main.ts
+++ lib/main.ts
@@ -1,17 +1,34 @@
 import * as TelegramBot from 'node-telegram-bot-api';
 import { Email } from './email';
 import { Gmail } from './gmail';
-import { Rooms } from './rooms';
+import { Apartament } from './rooms';
+import * as schedule from 'node-schedule';
+import { Bot } from './bot';
 
 export class Main {
 
-    private rooms: Rooms;
+    private rooms: Apartament;
     
     private email: Email;
     
     private gmail: Gmail;
 
+    private bot: Bot;
+
     constructor() {
-        this.rooms = new Rooms();
+        this.rooms = new Apartament();
+
+        this.bot = new Bot();
+
+        // For more information on cron jobs, visit this useful link
+        // https://crontab.guru/#1_0_*_*_*
+        //
+        schedule.scheduleJob('*/30 * * * *',  () => {
+            console.log('Running job...');
+            this.rooms.getNewApartaments().then(apartaments => {
+                console.log('Found new apartament');
+                apartaments.forEach(apartament => this.bot.sendApartamentSummary(apartament));
+            });
+        });
     }
 }
