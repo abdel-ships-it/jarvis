04c40fbe0c7dad3a2cade634449d215b8802c68d
Abdel El-Medny <abdel@springtree.nl>
2018-09-16 23:21:25 +0200

    chore(airbrake-js): adds airbrake-js for error monitoring

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ require('ts-node/register');
 const Main = require('./lib/main').Main;
 
 const main = new Main();
+
+var AirbrakeClient = require('airbrake-js');
+
+var airbrake = new AirbrakeClient({
+    projectId: 195863,
+    projectKey: '0f0293169f9fadde37747fa569b6694d'
+});
